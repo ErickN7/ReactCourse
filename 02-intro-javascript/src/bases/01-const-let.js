41ca56a19b0b3c264ed1f2e99df4225872c2bb53
Kubo Developer 7 <kubodeveloper7@outlook.com>
2022-12-06 18:04:28 -0600

    First commit

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+const name = "HOla";
+const lastname = "Mundo";
+
+let valueDado = 5;
+valueDado = 4;
+
+console.log(name, lastname, valueDado);
+
+if(true){
+    let valueDado = 6;
+
+    console.log(valueDado)
+}
+
+console.log(valueDado);
+
