e25235b727cca7f3f514ecc454af28f7486a8465
Kubo Developer 7 <kubodeveloper7@outlook.com>
2022-12-09 12:36:56 -0600

    changes

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,7 @@
-const arreglo = new Array[];
+// const arreglo = new Array(10);
+const arreglo = [1,23,4];
+// arreglo.push(1);
+
+let arreglo2 = [...arreglo,5];//clone array "arreglo" and added value 5
+console.log(arreglo);
+console.log(arreglo2);
